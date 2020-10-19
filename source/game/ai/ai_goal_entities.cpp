a7b158b8daa50403eb0eb8a6a783258e5c0f3dc0
Victor Luchits <vluchits@gmail.com>
2020-10-19 22:26:31 +0300

    Revert "Code refactoring"
    This reverts commit b9d418a8eca0a713600b7b69bd8df84999f01fce.

--- source/game/ai/ai_goal_entities.cpp
+++ source/game/ai/ai_goal_entities.cpp
@@ -21,9 +21,9 @@ float NavEntity::CostInfluence() const {
 		case IT_POWERUP:
 			return 0.6f;
 		case IT_ARMOR:
-			return ( itemTag != ARMOR_SHARD ) ? 0.7f : 0.9f;
+			return ( itemTag != (int)ARMOR_SHARD ) ? 0.7f : 0.9f;
 		case IT_HEALTH:
-			return ( itemTag == HEALTH_MEGA || itemTag == HEALTH_ULTRA ) ? 0.7f : 0.9f;
+			return ( itemTag == (int)HEALTH_MEGA || itemTag == (int)HEALTH_ULTRA ) ? 0.7f : 0.9f;
 		case IT_WEAPON:
 			return 0.8f;
 		case IT_AMMO:
@@ -45,7 +45,6 @@ bool NavEntity::IsTopTierItem( const float *overriddenEntityWeights ) const {
 
 	// Make these local copies to shorten lines.
 	const auto itemType = ent->item->type;
-
 	// Cast a enum value to int in comparisons to avoid analyzer warnings
 	// (different enum types are used for comparison).
 	const int itemTag = ent->item->tag;
@@ -54,20 +53,12 @@ bool NavEntity::IsTopTierItem( const float *overriddenEntityWeights ) const {
 		return true;
 	}
 
-	if( itemType == IT_HEALTH ) {
-		switch( itemTag ) {
-			case HEALTH_MEGA:
-			case HEALTH_ULTRA:
-				return true;
-		}	
+	if( itemType == IT_HEALTH && ( itemTag == (int)HEALTH_MEGA || itemTag == (int)HEALTH_ULTRA ) ) {
+		return true;
 	}
 
-	if( itemType == IT_ARMOR ) {
-		switch( itemTag ) {
-			case ARMOR_RA:
-			case ARMOR_YA:
-				return true;
-		}	
+	if( itemType == IT_ARMOR && ( itemTag == (int)ARMOR_RA || itemTag == (int)ARMOR_YA ) ) {
+		return true;
 	}
 
 	return false;
@@ -77,22 +68,18 @@ int64_t NavEntity::SpawnTime() const {
 	if( !ent->r.inuse ) {
 		return 0;
 	}
-
 	if( ent->r.solid == SOLID_TRIGGER ) {
 		return level.time;
 	}
-
 	// This means the nav entity is spawned by a script
 	// (only items are hardcoded nav. entities)
 	// Let the script manage the entity, do not prevent any action with the entity
 	if( !ent->item ) {
 		return level.time;
 	}
-
 	if( !ent->classname ) {
 		return 0;
 	}
-
 	// MH needs special handling
 	// If MH owner is sent, exact MH spawn time can't be predicted
 	// Otherwise fallback to the generic spawn prediction code below
@@ -100,7 +87,6 @@ int64_t NavEntity::SpawnTime() const {
 	if( ent->r.owner && !Q_stricmp( "item_health_mega", ent->classname ) ) {
 		return 0;
 	}
-
 	return ent->nextThink;
 }
 
@@ -111,7 +97,6 @@ uint64_t NavEntity::MaxWaitDuration() const {
 
 	// Make these local copies to shorten lines.
 	const auto itemType = ent->item->type;
-
 	// Cast a enum value to int in comparisons to avoid analyzer warnings
 	// (different enum types are used for comparison).
 	const int itemTag = ent->item->tag;
@@ -120,18 +105,19 @@ uint64_t NavEntity::MaxWaitDuration() const {
 		return 9000;
 	}
 
-	if( itemType == IT_HEALTH && ( itemTag == HEALTH_MEGA || itemTag == HEALTH_ULTRA ) ) {
+	if( itemType == IT_HEALTH && ( itemTag == (int)HEALTH_MEGA || itemTag == (int)HEALTH_ULTRA ) ) {
 		return 6000;
 	}
 
 	if( itemType == IT_ARMOR ) {
-		switch( itemTag ) {
-			case ARMOR_RA:
-				return 6000;
-			case ARMOR_YA:
-				return 5000;
-			case ARMOR_GA:
-				return 4000;
+		if( itemTag == (int)ARMOR_RA ) {
+			return 6000;
+		}
+		if( itemTag == (int)ARMOR_YA ) {
+			return 5000;
+		}
+		if( itemTag == (int)ARMOR_GA ) {
+			return 4000;
 		}
 	}
 
@@ -148,8 +134,6 @@ int64_t NavEntity::Timeout() const {
 NavEntitiesRegistry NavEntitiesRegistry::instance;
 
 void NavEntitiesRegistry::Init() {
-	unsigned i;
-
 	memset( navEntities, 0, sizeof( navEntities ) );
 	memset( entityToNavEntity, 0, sizeof( entityToNavEntity ) );
 
@@ -159,7 +143,7 @@ void NavEntitiesRegistry::Init() {
 	headnode.aasAreaNum = 0;
 	headnode.prev = &headnode;
 	headnode.next = &headnode;
-
+	unsigned i;
 	for( i = 0; i < sizeof( navEntities ) / sizeof( navEntities[0] ) - 1; i++ ) {
 		navEntities[i].id = i;
 		navEntities[i].next = &navEntities[i + 1];
@@ -173,7 +157,6 @@ void NavEntitiesRegistry::Init() {
 
 void NavEntitiesRegistry::Update() {
 	AiAasWorld *aasWorld = AiAasWorld::Instance();
-
 	FOREACH_NAVENT( navEnt )
 	{
 		if( ( navEnt->flags & NavEntityFlags::MOVABLE ) != NavEntityFlags::NONE ) {
@@ -200,7 +183,6 @@ NavEntity *NavEntitiesRegistry::AllocNavEntity() {
 
 NavEntity *NavEntitiesRegistry::AddNavEntity( edict_t *ent, int aasAreaNum, NavEntityFlags flags ) {
 	NavEntity *navEntity = AllocNavEntity();
-
 	if( navEntity ) {
 		int entNum = ENTNUM( ent );
 		navEntity->ent = ent;
@@ -212,7 +194,6 @@ NavEntity *NavEntitiesRegistry::AddNavEntity( edict_t *ent, int aasAreaNum, NavE
 		entityToNavEntity[entNum] = navEntity;
 		return navEntity;
 	}
-
 	constexpr const char *format = S_COLOR_RED "Can't allocate a nav. entity for %s @ %.3f %.3f %.3f\n";
 	G_Printf( format, ent->classname, ent->s.origin[0], ent->s.origin[1], ent->s.origin[2] );
 	return nullptr;
@@ -220,7 +201,6 @@ NavEntity *NavEntitiesRegistry::AddNavEntity( edict_t *ent, int aasAreaNum, NavE
 
 void NavEntitiesRegistry::RemoveNavEntity( NavEntity *navEntity ) {
 	edict_t *ent = navEntity->ent;
-
 	FreeNavEntity( navEntity );
 	entityToNavEntity[ENTNUM( ent )] = nullptr;
 }
